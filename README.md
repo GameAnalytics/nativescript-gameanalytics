b06979ae568f36016c66b1dcec5a44519c465db9
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2018-05-25 16:05:37 +0200

    Updated to v1.1.4

--- README.md
+++ README.md
@@ -44,6 +44,9 @@ For more documentation click [here](https://gameanalytics.com/docs/nativescript-
 Changelog
 ---------
 <!--(CHANGELOG_TOP)-->
+**1.1.4**
+* small fix to gradle file
+
 **1.1.3**
 * README updated
 
--- src/gameanalytics.android.ts
+++ src/gameanalytics.android.ts
@@ -6,7 +6,7 @@ declare var java: any;
 const GameAnalytics:any = com.gameanalytics.sdk.GameAnalytics;
 
 export class GameAnalyticsSDK {
-    private static version:string = "1.1.3";
+    private static version:string = "1.1.4";
     private static _onCommandCenterUpdated:Array<() => void> = new Array<() => void>();
 
     private static CommandCenterListenerImpl = java.lang.Object.extend({
